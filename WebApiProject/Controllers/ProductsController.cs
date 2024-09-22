b43b0c2333c6a4719a6cbdd812f7ed2ff0e9a8a6
giraycelenk <a.giraycelenk@hotmail.com>
2024-09-22 16:56:43 +0300

    Product seed data added and methods changed

--- WebApiProject/Controllers/ProductsController.cs
+++ WebApiProject/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApiProject.Models;
 
 namespace WebApiProject.Controllers
 {
@@ -7,21 +8,29 @@ namespace WebApiProject.Controllers
     [Route("api/[controller]")]
     public class ProductsController:ControllerBase
     {
-        private static readonly string[] Products = {
-            "IPhone 14","IPhone 15","IPhone 16"
-        };
-            
+        private static List<Product>? _products;
+        
+        public ProductsController()
+        {
+            _products =
+            [
+                new Product{ ProductId = 1, ProductName = "IPhone 14",Price=46000, IsActive = true},
+                new Product{ ProductId = 2, ProductName = "IPhone 15",Price=56000, IsActive = true},
+                new Product{ ProductId = 3, ProductName = "IPhone 16",Price=66000, IsActive = true},
+                new Product{ ProductId = 4, ProductName = "IPhone 17",Price=76000, IsActive = false},
+            ];
+        }
         
         [HttpGet]
-        public string[] GetProducts()
+        public List<Product> GetProducts()
         {
-            return Products;
+            return _products ?? new List<Product>();
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public Product? GetProduct(int id)
         {
-            return Products[id];
+            return _products?.FirstOrDefault(x => x.ProductId == id) ?? new Product();
         }
     }
 }
---
+++ WebApiProject/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace WebApiProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
