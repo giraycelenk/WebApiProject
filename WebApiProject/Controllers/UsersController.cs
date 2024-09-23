6a2a4743c3f8724dc15a07c5db9d1958b008c839
giraycelenk <a.giraycelenk@hotmail.com>
2024-09-23 22:43:37 +0300

    User register method added

---
+++ WebApiProject/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProject.DTO;
+using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
+    {
+        private UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager; 
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> CreateUser(UserDTO model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new AppUser
+            {
+                FullName = model.FullName,
+                UserName = model.UserName,
+                Email = model.Email,
+                DateAdded = DateTime.Now
+            };
+            
+            var result = await _userManager.CreateAsync(user,model.Password);
+
+            if(result.Succeeded)
+            {
+                return StatusCode(201);
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}
