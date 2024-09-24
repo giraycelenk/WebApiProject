fcef4a35ec8c9910766c30ba7adb2301704ea3ec
giraycelenk <a.giraycelenk@hotmail.com>
2024-09-24 21:48:37 +0300

    User login method added

--- WebApiProject/Controllers/UsersController.cs
+++ WebApiProject/Controllers/UsersController.cs
@@ -10,10 +10,12 @@ namespace WebApiProject.Controllers
     public class UsersController:ControllerBase
     {
         private UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public UsersController(UserManager<AppUser> userManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager; 
+            _signInManager = signInManager;
         }
 
         [HttpPost("Register")]
@@ -38,6 +40,25 @@ namespace WebApiProject.Controllers
                 return StatusCode(201);
             }
             return BadRequest(result.Errors);
+        }
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user == null)
+            {
+                return BadRequest(new { message = "email hatalı" });
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user,model.Password,false);
+
+            if(result.Succeeded)
+            {
+                return Ok(
+                    new { token = "token" }
+                );
+            }
+            return Unauthorized();
         }
     }
 }
